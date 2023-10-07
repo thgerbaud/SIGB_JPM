a54b880a73258d7dfa13dcb35fc4a3dcaae923c7
Thomas Gerbaud <tgerbaud@enssat.fr>
2023-10-07 15:52:28 -0400

    LibrariesList : modification du style

--- frontend/src/components/LibrariesList.vue
+++ frontend/src/components/LibrariesList.vue
@@ -1,22 +1,28 @@
 <template>
-	<div>
-		<h1>Bienvenue, {{ user }}</h1>
-		<h2>Mes bibliothèques :</h2>
-		<div v-if="libraries.length == 0">Aucune bibliothèque pour le moment</div>
-		<div v-else>
-			<ul>
-				<li v-for="(library, index) in libraries" :key="index">
+	<main>
+		<h2>Bienvenue, {{ user }}</h2>
+		<section id="libraries-view">
+			<h1>Mes bibliothèques :</h1>
+			<div v-if="libraries.length == 0">Aucune bibliothèque pour le moment</div>
+			<div v-else id="libraries-list">
+				<router-link v-for="(library, index) in libraries" :key="index" class="library-item" :to="`/${library._id}/books`">
+					<div class="library-name">{{ library.name }}</div>
+					<div>></div>
+				</router-link>
+				<!--
+				<div v-for="(library, index) in libraries" :key="index" class="library-item">
 					<router-link :to="`/${library._id}/books`">{{ library.name }}</router-link>
-				</li>
-			</ul>
-		</div>
-		<router-link :to="`/${user}/create`">
-			<button>+ créer une bibliothèque</button>
-		</router-link>
+				</div>
+			-->
+				<router-link :to="`/${user}/create`" id="create-library-section">
+					<button id="create-library-btn" class="secondary">+ créer une bibliothèque</button>
+				</router-link>
+			</div>
+		</section>
 		<router-link to="/">
-			<button>Déconnexion</button>
+			<button class="tertiary">Déconnexion</button>
 		</router-link>
-	</div>
+	</main>
 </template>
 
 <script>
@@ -62,3 +68,43 @@ export default {
 	}
 };
 </script>
+
+<style>
+#libraries-view {
+	min-width: 300px;
+}
+
+#libraries-list {
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+}
+
+.library-item {
+	font-size: 1.5rem;
+	padding: 0.5rem 1rem;
+	border-radius: 5px;
+	background-color: rgba(228, 228, 228, 0.5);
+	border: 1px solid rgb(150, 150, 150);
+	color: black;
+	text-decoration: none;
+	display: flex;
+}
+
+.library-item:hover {
+	box-shadow: 0 2px 5px rgb(150, 150, 150);
+}
+
+.library-name {
+	flex-grow: 1;
+}
+
+#create-library-section {
+	display: flex;
+	text-decoration: none;
+}
+
+#create-library-btn {
+	flex-grow: 1;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,5 @@
 <template>
-    <main>
-        <router-view></router-view>
-    </main>
+    <router-view></router-view>
 </template>
 
-<style>
-</style>
+<style></style>
