f81b012645a0641aac9add0fec5b381f6c7216a5
Thomas Gerbaud <tgerbaud@enssat.fr>
2023-10-18 17:19:55 -0400

    .env backend

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://boisgomin:nk5wd7DPQdnLfLOj@cluster0.thhic6a.mongodb.net/?retryWrites=true&w=majority"
+    url: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PWD}@cluster0.thhic6a.mongodb.net/?retryWrites=true&w=majority`
 }
